7fa49205d0840994d59844766b53ed8ebb3c84cd
Tamires <112409145+Tamiresporto@users.noreply.github.com>
2022-09-22 16:21:57 -0300

    Edição com JavaScript

--- microempreendedor/scriptContato/contato.js
+++ microempreendedor/scriptContato/contato.js
@@ -1,6 +1,18 @@
-function myFunction() {
-    document.getElementById("mensagem").innerHTML = "Mensagem Enviada! Obrigada por entrar em contato conosco.";
-  }
   function comprar() {
     alert("PARABÉNS! COMPRA REALIZADA. OBRIGADA POR COMPRAR CONOSCO!")
  }
+
+ function validar() {
+  let campo_nome = formulario.nome;
+  
+  if(nome.value =="") {
+     alert("Nome não informado");
+
+    nome.focus();
+
+  } else { 
+   (nome.value ="")
+    alert("Mensagem Enviada! Obrigada por entrar em contato conosco.");
+    nome.focus();
+  }
+}
