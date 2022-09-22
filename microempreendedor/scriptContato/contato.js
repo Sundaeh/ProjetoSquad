a9a064d907ed54695303eab83d65c33d9be01c0e
Tamires <112409145+Tamiresporto@users.noreply.github.com>
2022-09-22 00:08:31 -0300

    Site 2.2
    - Botão de comprar no menu inicial inserido

--- microempreendedor/scriptContato/contato.js
+++ microempreendedor/scriptContato/contato.js
@@ -1,3 +1,6 @@
 function myFunction() {
     document.getElementById("mensagem").innerHTML = "Mensagem Enviada! Obrigada por entrar em contato conosco.";
   }
+  function comprar() {
+    alert("PARABÉNS! COMPRA REALIZADA. OBRIGADA POR COMPRAR CONOSCO!")
+ }
